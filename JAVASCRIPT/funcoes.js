5dbda96aba47c523c33c35731c5a3b1817d3d57b
Bárbara Menezes <barbaraeduardamenezes@gmail.com>
2022-08-16 20:29:41 -0300

    first commit

---
+++ JAVASCRIPT/funcoes.js
@@ -0,0 +1,65 @@
+
+function somaChocolate(){
+        var val1 = parseInt(document.getElementById("chocolate1").value);
+        var val2 = parseInt(document.getElementById("chocolate2").value);
+        var val3 = parseInt(document.getElementById("chocolate3").value);
+        var val4 = parseInt(document.getElementById("chocolate4").value);
+        
+
+
+        var total = ((val1*15)+(val2*25) + (val3*5)+(val4*3));
+
+        
+        document.getElementById('tela').setAttribute("value", total);
+
+      
+    }
+
+function somaMorango(){
+        var val1 = parseInt(document.getElementById("morango1").value);
+        var val2 = parseInt(document.getElementById("morango2").value);
+        var val3 = parseInt(document.getElementById("morango3").value);
+        var val4 = parseInt(document.getElementById("morango4").value);
+        
+
+
+        var total = ((val1*15)+(val2*25) + (val3*5)+(val4*3));
+
+        
+        document.getElementById('tela').setAttribute("value", total);
+
+      
+    }
+
+    
+function somaCreme(){
+        var val1 = parseInt(document.getElementById("creme1").value);
+        var val2 = parseInt(document.getElementById("creme2").value);
+        var val3 = parseInt(document.getElementById("creme3").value);
+        var val4 = parseInt(document.getElementById("creme4").value);
+        
+
+
+        var total = ((val1*15)+(val2*25) + (val3*5)+(val4*3));
+
+        
+        document.getElementById('tela').setAttribute("value", total);
+
+      
+    }
+
+function somaFrutas(){
+        var val1 = parseInt(document.getElementById("fruta1").value);
+        var val2 = parseInt(document.getElementById("fruta2").value);
+        var val3 = parseInt(document.getElementById("fruta3").value);
+        var val4 = parseInt(document.getElementById("fruta4").value);
+        
+
+
+        var total = ((val1*15)+(val2*25) + (val3*5)+(val4*3));
+
+        
+        document.getElementById('tela').setAttribute("value", total);
+
+      
+    }
